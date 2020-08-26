729d2cf1c35af6bfdd7400c36926bd8ee77339cc
Hang <chun0153@flinders.edu.au>
2020-08-26 10:59:10 +0930

    unit3

--- unit3/checkpoint3.1.asm
+++ unit3/checkpoint3.1.asm
@@ -5,9 +5,9 @@
 .segmentdef Data [startAfter="Code", min=$8200, max=$bdff]
 .segmentdef Stack [min=$be00, max=$beff, fill]
 .segmentdef Zeropage [min=$bf00, max=$bfff, fill]
-  .label RASTER = $d012
-  .label BGCOL = $d021
-  .const BLACK = 0
+  .label VIC_MEMORY = $d018
+  .label SCREEN = $400
+  .label COLS = $d800
   .const WHITE = 1
   .const JMP = $4c
   .const NOP = $ea
@@ -49,20 +49,68 @@ pagfault: {
     rts
 }
 reset: {
-  __b1:
-    lda #$36
-    cmp RASTER
-    beq __b2
-    lda #$42
-    cmp RASTER
-    beq __b2
-    lda #BLACK
-    sta BGCOL
-    jmp __b1
+    lda #$14
+    sta VIC_MEMORY
+    ldx #' '
+    lda #<SCREEN
+    sta.z memset.str
+    lda #>SCREEN
+    sta.z memset.str+1
+    lda #<$28*$19
+    sta.z memset.num
+    lda #>$28*$19
+    sta.z memset.num+1
+    jsr memset
+    ldx #WHITE
+    lda #<COLS
+    sta.z memset.str
+    lda #>COLS
+    sta.z memset.str+1
+    lda #<$28*$19
+    sta.z memset.num
+    lda #>$28*$19
+    sta.z memset.num+1
+    jsr memset
+    jsr exit_hypervisor
+    rts
+}
+// Copies the character c (an unsigned char) to the first num characters of the object pointed to by the argument str.
+// memset(void* zeropage(4) str, byte register(X) c, word zeropage(2) num)
+memset: {
+    .label end = 2
+    .label dst = 4
+    .label num = 2
+    .label str = 4
+    lda.z num
+    bne !+
+    lda.z num+1
+    beq __breturn
+  !:
+    lda.z end
+    clc
+    adc.z str
+    sta.z end
+    lda.z end+1
+    adc.z str+1
+    sta.z end+1
   __b2:
-    lda #WHITE
-    sta BGCOL
-    jmp __b1
+    lda.z dst+1
+    cmp.z end+1
+    bne __b3
+    lda.z dst
+    cmp.z end
+    bne __b3
+  __breturn:
+    rts
+  __b3:
+    txa
+    ldy #0
+    sta (dst),y
+    inc.z dst
+    bne !+
+    inc.z dst+1
+  !:
+    jmp __b2
 }
 syscall3F: {
     jsr exit_hypervisor
