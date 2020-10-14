6e9ad747c40ac8dab99008d33d047dd08ca92446
Hang <chun0153@flinders.edu.au>
2020-10-14 11:47:28 +1030

    unit4

---
+++ unit4/checkpoint4.5.asm
@@ -0,0 +1,648 @@
+  .file [name="checkpoint4.5.bin", type="bin", segments="XMega65Bin"]
+.segmentdef XMega65Bin [segments="Syscall, Code, Data, Stack, Zeropage"]
+.segmentdef Syscall [start=$8000, max=$81ff]
+.segmentdef Code [start=$8200, min=$8200, max=$bdff]
+.segmentdef Data [startAfter="Code", min=$8200, max=$bdff]
+.segmentdef Stack [min=$be00, max=$beff, fill]
+.segmentdef Zeropage [min=$bf00, max=$bfff, fill]
+  .label VIC_MEMORY = $d018
+  .label SCREEN = $400
+  .label COLS = $d800
+  .const WHITE = 1
+  .const JMP = $4c
+  .const NOP = $ea
+  .label current_screen_line = 8
+  .label current_screen_x = $a
+  lda #<0
+  sta.z current_screen_line
+  sta.z current_screen_line+1
+  sta.z current_screen_x
+  jsr main
+  rts
+.segment Code
+main: {
+    rts
+}
+undefined_trap: {
+    jsr exit_hypervisor
+    rts
+}
+exit_hypervisor: {
+    lda #1
+    sta $d67f
+    rts
+}
+cpukil: {
+    jsr exit_hypervisor
+    rts
+}
+reserved: {
+    jsr exit_hypervisor
+    rts
+}
+vf011wr: {
+    jsr exit_hypervisor
+    rts
+}
+vf011rd: {
+    jsr exit_hypervisor
+    rts
+}
+alttabkey: {
+    jsr exit_hypervisor
+    rts
+}
+restorkey: {
+    jsr exit_hypervisor
+    rts
+}
+pagfault: {
+    jsr exit_hypervisor
+    rts
+}
+//void syscall63(){exit_hypervisor ();}
+//void syscall64(){exit_hypervisor ();}
+reset: {
+    lda #$14
+    sta VIC_MEMORY
+    ldx #' '
+    lda #<SCREEN
+    sta.z memset.str
+    lda #>SCREEN
+    sta.z memset.str+1
+    lda #<$28*$19
+    sta.z memset.num
+    lda #>$28*$19
+    sta.z memset.num+1
+    jsr memset
+    ldx #WHITE
+    lda #<COLS
+    sta.z memset.str
+    lda #>COLS
+    sta.z memset.str+1
+    lda #<$28*$19
+    sta.z memset.num
+    lda #>$28*$19
+    sta.z memset.num+1
+    jsr memset
+    lda #<$400
+    sta.z current_screen_line
+    lda #>$400
+    sta.z current_screen_line+1
+    lda #0
+    sta.z current_screen_x
+    lda #<message
+    sta.z print_to_screen.message
+    lda #>message
+    sta.z print_to_screen.message+1
+    jsr print_to_screen
+    jsr start_simple_program
+    jsr exit_hypervisor
+    rts
+  .segment Data
+    message: .text "checkpoint4.5 by chun0153"
+    .byte 0
+}
+.segment Code
+start_simple_program: {
+    lda #$80
+    sta $300
+    lda #0
+    sta $301
+    lda #$81
+    sta $302
+    lda #0
+    sta $303
+    sta $304
+    sta $305
+    sta $306
+    lda #$60
+    sta $307
+    lda #2
+    sta $308
+    lda #0
+    sta $309
+    lda #2
+    sta $30a
+    lda #1
+    sta $30b
+    lda #8
+    sta $30c
+    lda #0
+    sta $30d
+    sta $30e
+    sta $30f
+    lda #$60
+    sta $310
+    lda #3
+    sta $d701
+    lda #0
+    sta $d702
+    sta $d705
+    lda #<$80d
+    sta $d648
+    lda #>$80d
+    sta $d648+1
+    jsr exit_hypervisor
+    rts
+}
+// print_to_screen(byte* zeropage(2) message)
+print_to_screen: {
+    .label message = 2
+  __b1:
+    ldy #0
+    lda (message),y
+    cmp #0
+    bne __b2
+    rts
+  __b2:
+    ldy #0
+    lda (message),y
+    ldy.z current_screen_x
+    sta (current_screen_line),y
+    inc.z message
+    bne !+
+    inc.z message+1
+  !:
+    inc.z current_screen_x
+    jmp __b1
+}
+// Copies the character c (an unsigned char) to the first num characters of the object pointed to by the argument str.
+// memset(void* zeropage(6) str, byte register(X) c, word zeropage(4) num)
+memset: {
+    .label end = 4
+    .label dst = 6
+    .label num = 4
+    .label str = 6
+    lda.z num
+    bne !+
+    lda.z num+1
+    beq __breturn
+  !:
+    lda.z end
+    clc
+    adc.z str
+    sta.z end
+    lda.z end+1
+    adc.z str+1
+    sta.z end+1
+  __b2:
+    lda.z dst+1
+    cmp.z end+1
+    bne __b3
+    lda.z dst
+    cmp.z end
+    bne __b3
+  __breturn:
+    rts
+  __b3:
+    txa
+    ldy #0
+    sta (dst),y
+    inc.z dst
+    bne !+
+    inc.z dst+1
+  !:
+    jmp __b2
+}
+syscall62: {
+    jsr exit_hypervisor
+    rts
+}
+syscall61: {
+    jsr exit_hypervisor
+    rts
+}
+syscall60: {
+    jsr exit_hypervisor
+    rts
+}
+syscall59: {
+    jsr exit_hypervisor
+    rts
+}
+syscall58: {
+    jsr exit_hypervisor
+    rts
+}
+syscall57: {
+    jsr exit_hypervisor
+    rts
+}
+syscall56: {
+    jsr exit_hypervisor
+    rts
+}
+syscall55: {
+    jsr exit_hypervisor
+    rts
+}
+syscall54: {
+    jsr exit_hypervisor
+    rts
+}
+syscall53: {
+    jsr exit_hypervisor
+    rts
+}
+syscall52: {
+    jsr exit_hypervisor
+    rts
+}
+syscall51: {
+    jsr exit_hypervisor
+    rts
+}
+syscall50: {
+    jsr exit_hypervisor
+    rts
+}
+syscall49: {
+    jsr exit_hypervisor
+    rts
+}
+syscall48: {
+    jsr exit_hypervisor
+    rts
+}
+syscall47: {
+    jsr exit_hypervisor
+    rts
+}
+syscall46: {
+    jsr exit_hypervisor
+    rts
+}
+syscall45: {
+    jsr exit_hypervisor
+    rts
+}
+syscall44: {
+    jsr exit_hypervisor
+    rts
+}
+syscall43: {
+    jsr exit_hypervisor
+    rts
+}
+syscall42: {
+    jsr exit_hypervisor
+    rts
+}
+syscall41: {
+    jsr exit_hypervisor
+    rts
+}
+syscall40: {
+    jsr exit_hypervisor
+    rts
+}
+syscall39: {
+    jsr exit_hypervisor
+    rts
+}
+syscall38: {
+    jsr exit_hypervisor
+    rts
+}
+syscall37: {
+    jsr exit_hypervisor
+    rts
+}
+syscall36: {
+    jsr exit_hypervisor
+    rts
+}
+syscall35: {
+    jsr exit_hypervisor
+    rts
+}
+syscall34: {
+    jsr exit_hypervisor
+    rts
+}
+syscall33: {
+    jsr exit_hypervisor
+    rts
+}
+syscall32: {
+    jsr exit_hypervisor
+    rts
+}
+syscall31: {
+    jsr exit_hypervisor
+    rts
+}
+syscall30: {
+    jsr exit_hypervisor
+    rts
+}
+syscall29: {
+    jsr exit_hypervisor
+    rts
+}
+syscall28: {
+    jsr exit_hypervisor
+    rts
+}
+syscall27: {
+    jsr exit_hypervisor
+    rts
+}
+syscall26: {
+    jsr exit_hypervisor
+    rts
+}
+syscall25: {
+    jsr exit_hypervisor
+    rts
+}
+syscall24: {
+    jsr exit_hypervisor
+    rts
+}
+syscall23: {
+    jsr exit_hypervisor
+    rts
+}
+syscall22: {
+    jsr exit_hypervisor
+    rts
+}
+syscall21: {
+    jsr exit_hypervisor
+    rts
+}
+syscall20: {
+    jsr exit_hypervisor
+    rts
+}
+syscall19: {
+    jsr exit_hypervisor
+    rts
+}
+securexit: {
+    jsr exit_hypervisor
+    rts
+}
+securentr: {
+    jsr exit_hypervisor
+    rts
+}
+syscall16: {
+    jsr exit_hypervisor
+    rts
+}
+syscall15: {
+    jsr exit_hypervisor
+    rts
+}
+syscall14: {
+    jsr exit_hypervisor
+    rts
+}
+syscall13: {
+    jsr exit_hypervisor
+    rts
+}
+syscall12: {
+    jsr exit_hypervisor
+    rts
+}
+syscall11: {
+    jsr exit_hypervisor
+    rts
+}
+syscall10: {
+    jsr exit_hypervisor
+    rts
+}
+syscall9: {
+    jsr exit_hypervisor
+    rts
+}
+syscall8: {
+    jsr exit_hypervisor
+    rts
+}
+syscall7: {
+    jsr exit_hypervisor
+    rts
+}
+syscall6: {
+    jsr exit_hypervisor
+    rts
+}
+syscall5: {
+    jsr exit_hypervisor
+    rts
+}
+syscall03: {
+    // *(SCREEN+78) = '<';
+    .label message = $303
+    jsr print_newline
+    lda #<message
+    sta.z print_to_screen.message
+    lda #>message
+    sta.z print_to_screen.message+1
+    jsr print_to_screen
+    jsr exit_hypervisor
+    rts
+}
+print_newline: {
+    lda #$28
+    clc
+    adc.z current_screen_line
+    sta.z current_screen_line
+    bcc !+
+    inc.z current_screen_line+1
+  !:
+    lda #0
+    sta.z current_screen_x
+    rts
+}
+syscall02: {
+    // *(SCREEN+78) = '<';
+    .label message = $302
+    jsr print_newline
+    lda #<message
+    sta.z print_to_screen.message
+    lda #>message
+    sta.z print_to_screen.message+1
+    jsr print_to_screen
+    jsr exit_hypervisor
+    rts
+}
+syscall01: {
+    jsr print_newline
+    lda #<message
+    sta.z print_to_screen.message
+    lda #>message
+    sta.z print_to_screen.message+1
+    jsr print_to_screen
+    jsr exit_hypervisor
+    rts
+  .segment Data
+    message: .text "syscall01 entered"
+    .byte 0
+}
+.segment Code
+syscall00: {
+    jsr print_newline
+    lda #<message
+    sta.z print_to_screen.message
+    lda #>message
+    sta.z print_to_screen.message+1
+    jsr print_to_screen
+    jsr exit_hypervisor
+    rts
+  .segment Data
+    message: .text "syscall00 entered"
+    .byte 0
+}
+.segment Syscall
+  SYSCALLS: .byte JMP
+  .word syscall00
+  .byte NOP, JMP
+  .word syscall01
+  .byte NOP, JMP
+  .word syscall02
+  .byte NOP, JMP
+  .word syscall03
+  .byte NOP, JMP
+  .word syscall5
+  .byte NOP, JMP
+  .word syscall6
+  .byte NOP, JMP
+  .word syscall7
+  .byte NOP, JMP
+  .word syscall8
+  .byte NOP, JMP
+  .word syscall9
+  .byte NOP, JMP
+  .word syscall10
+  .byte NOP, JMP
+  .word syscall11
+  .byte NOP, JMP
+  .word syscall12
+  .byte NOP, JMP
+  .word syscall13
+  .byte NOP, JMP
+  .word syscall14
+  .byte NOP, JMP
+  .word syscall15
+  .byte NOP, JMP
+  .word syscall16
+  .byte NOP, JMP
+  .word securentr
+  .byte NOP, JMP
+  .word securexit
+  .byte NOP, JMP
+  .word syscall19
+  .byte NOP, JMP
+  .word syscall20
+  .byte NOP, JMP
+  .word syscall21
+  .byte NOP, JMP
+  .word syscall22
+  .byte NOP, JMP
+  .word syscall23
+  .byte NOP, JMP
+  .word syscall24
+  .byte NOP, JMP
+  .word syscall25
+  .byte NOP, JMP
+  .word syscall26
+  .byte NOP, JMP
+  .word syscall27
+  .byte NOP, JMP
+  .word syscall28
+  .byte NOP, JMP
+  .word syscall29
+  .byte NOP, JMP
+  .word syscall30
+  .byte NOP, JMP
+  .word syscall31
+  .byte NOP, JMP
+  .word syscall32
+  .byte NOP, JMP
+  .word syscall33
+  .byte NOP, JMP
+  .word syscall34
+  .byte NOP, JMP
+  .word syscall35
+  .byte NOP, JMP
+  .word syscall36
+  .byte NOP, JMP
+  .word syscall37
+  .byte NOP, JMP
+  .word syscall38
+  .byte NOP, JMP
+  .word syscall39
+  .byte NOP, JMP
+  .word syscall40
+  .byte NOP, JMP
+  .word syscall41
+  .byte NOP, JMP
+  .word syscall42
+  .byte NOP, JMP
+  .word syscall43
+  .byte NOP, JMP
+  .word syscall44
+  .byte NOP, JMP
+  .word syscall45
+  .byte NOP, JMP
+  .word syscall46
+  .byte NOP, JMP
+  .word syscall47
+  .byte NOP, JMP
+  .word syscall48
+  .byte NOP, JMP
+  .word syscall49
+  .byte NOP, JMP
+  .word syscall50
+  .byte NOP, JMP
+  .word syscall51
+  .byte NOP, JMP
+  .word syscall52
+  .byte NOP, JMP
+  .word syscall53
+  .byte NOP, JMP
+  .word syscall54
+  .byte NOP, JMP
+  .word syscall55
+  .byte NOP, JMP
+  .word syscall56
+  .byte NOP, JMP
+  .word syscall57
+  .byte NOP, JMP
+  .word syscall58
+  .byte NOP, JMP
+  .word syscall59
+  .byte NOP, JMP
+  .word syscall60
+  .byte NOP, JMP
+  .word syscall61
+  .byte NOP, JMP
+  .word syscall62
+  .byte NOP
+  .align $100
+  TRAPS: .byte JMP
+  .word reset
+  .byte NOP, JMP
+  .word pagfault
+  .byte NOP, JMP
+  .word restorkey
+  .byte NOP, JMP
+  .word alttabkey
+  .byte NOP, JMP
+  .word vf011rd
+  .byte NOP, JMP
+  .word vf011wr
+  .byte NOP, JMP
+  .word reserved
+  .byte NOP, JMP
+  .word cpukil
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP
